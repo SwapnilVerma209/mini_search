32f1a63228c2b63c28f3accd1990af5e2f6713c1
Swapnil-Verma <verma119@purdue.edu>
2025-07-23 18:20:50 -0700

    Removed stray print statement from tokenizer test context file.

--- src/test/tokenizers/context.py
+++ src/test/tokenizers/context.py
@@ -4,6 +4,5 @@ dirname = os.path.dirname(__file__)
 rel_path = os.path.join(dirname, '../..')
 abs_path = os.path.abspath(rel_path)
 sys.path.insert(0, abs_path)
-print(abs_path)
 
 import tokenizers
